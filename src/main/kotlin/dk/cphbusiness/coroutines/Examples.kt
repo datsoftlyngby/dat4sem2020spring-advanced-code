39d424dfdac51ff8855df991fad7db90344f7c36
eguahlak <anders@kalhauge.dk>
2020-03-24 12:27:29 +0100

    coroutines and threads

--- src/main/kotlin/dk/cphbusiness/coroutines/Examples.kt
+++ src/main/kotlin/dk/cphbusiness/coroutines/Examples.kt
@@ -1,11 +1,9 @@
 package dk.cphbusiness.coroutines
 
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import java.lang.Thread.sleep
 import kotlin.concurrent.thread
+import kotlin.system.measureTimeMillis
 
 fun main_example_1() {
   GlobalScope.launch {
@@ -17,10 +15,56 @@ fun main_example_1() {
   runBlocking { delay(2000) }
   }
 
-fun main() = runBlocking {
+fun main_example_2() = runBlocking {
   launch {
     delay(1000)
     println(" World!")
     }
   print("Hello")
+  }
+
+fun main_example_3() {
+  val result: Deferred<String> = GlobalScope.async {
+    delay(1000)
+    "World"
+    }
+  runBlocking {
+    val who = result.await()
+    println("Hello $who")
+    }
+  }
+
+fun main_example_4() = runBlocking {
+  val result = async { delay(1000); "World!" }
+  println("Hello ${result.await()}")
+  }
+
+fun run_threads(max: Int) {
+  for (index in 0..max) {
+    thread {
+      sleep(100)
+      if (index%(max/10) == 0) println("running thread $index")
+      }
+    }
+  }
+
+fun run_coroutines(max: Int) = runBlocking {
+  for (index in 0..max) {
+    launch {
+      delay(100)
+      if (index%(max/10) == 0) println("running coroutine $index")
+      }
+    }
+  }
+
+fun main() {
+  val count = 1_000_000
+  val mc = measureTimeMillis {
+    run_coroutines(count)
+    }
+  println("Coroutines: $mc")
+  val mt = measureTimeMillis {
+    run_threads(count)
+    }
+  println("Threads: $mt")
   }
